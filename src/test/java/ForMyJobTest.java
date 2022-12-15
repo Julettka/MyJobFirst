0e478d5efc31056e75eb2636773631b775a29003
Julia <JuliavorobejVs@gmail.com>
2022-12-15 14:39:39 +0300

    Initial commit

---
+++ src/test/java/ForMyJobTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ForMyJobTest {
+    public static void main(String[] args) {
+        jobTest();
+    }
+
+    public static void jobTest() {
+        String s = "Hello World";
+        assertEquals(s, "Hello World");
+    }
+}
