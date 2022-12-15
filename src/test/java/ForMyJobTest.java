627307e692175620d42a8e8b5350f8a87bf403ee
Julia <JuliavorobejVs@gmail.com>
2022-12-15 17:19:10 +0300

    second commit

--- src/test/java/ForMyJobTest.java
+++ src/test/java/ForMyJobTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ForMyJobTest {
     public static void main(String[] args) {
         jobTest();
+        System.out.println("QQQ");
     }
 
     public static void jobTest() {
